4b6b6defd9a2f5e07e437fcc8342be196e7ccb09
berkutsoft <berkut1011@gmail.com>
2023-12-21 10:30:51 +0300

    fix MANIFEST.in

--- setup.py
+++ setup.py
@@ -7,16 +7,18 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="streamlit-telegram-login",
-    version="0.0.2",
+    version="0.0.3",
     author="Berkutsoft 11",
     author_email="berkut1011@gmail.com",
     description="Streamlit component that implements a telegram bot widget",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    url="https://github.com/berkutsoft/streamlit-telegram-login",
     packages=setuptools.find_packages(),
     include_package_data=True,
     classifiers=[],
     python_requires=">=3.8",
+    license_files=("LICENSE",),
     install_requires=[
         "streamlit>=1.28.0",
         "jinja2",
